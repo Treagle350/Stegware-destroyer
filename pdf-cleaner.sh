874f932a45560fee9311b2f7525efd926d774784
Treagle350 <treagle350@gmail.com>
2024-02-18 18:18:40 +0100

    Added filename cleaning
    Added filename cleaning that removes multiple extensions and verifies extensions so that they match with the files.

--- pdf-cleaner.sh
+++ pdf-cleaner.sh
@@ -1,23 +1,37 @@
 #! /bin/bash
 
-mkdir "/tmp/temp-imgs-pdf-conversion"
-while [[ $# -ne 0 ]]; do
-	current_index_of_shell_parameter_expansion=$1
+if [[ "$EUID" = 0 ]]; then
+        echo "Never use sudo or a root account for trivial stuff, you're inviting disasters and viruses in..."
+        echo "Program is quitting and didn't execute anything..."
+        exit
+fi
 
-        #Strip given filename of its path
-        current_file=${current_index_of_shell_parameter_expansion##*/}
+tmp_img_proc_dir="/tmp/temp-imgs-pdf-conversion"
+mkdir "${tmp_img_proc_dir}"
+running_path_of_script="$(dirname $0)"
+
+while [[ $# -ne 0 ]]; do
+	#Somewhat on the fence over this because this might be able to run commands if something is injected under var running_path, the only thing stopping that from happening is the curly braces around it
+	current_index_of_shell_parameter_expansion="$(${running_path_of_script}/file-name-cleaner.sh ${1} | head -1)"
+
+	#Safer alternative than ommitting path part via expression
+	current_file="$(basename ${current_index_of_shell_parameter_expansion})"
 
 	echo "Converting PDF file: $current_file into multiple PNG files..."
 
 	#PNG files have less complicated metadata fields so less attack surface
-	pdftoppm "$1" "/tmp/temp-imgs-pdf-conversion/out-$current_file" -png
+	pdftoppm "${current_index_of_shell_parameter_expansion}" "${tmp_img_proc_dir}/out-${current_file}" -png
 
 	echo "Cleaning individual PNG files..."
 
-	./ffmpeg-file-cleaner.sh "/tmp/temp-imgs-pdf-conversion/out-$current_file"*".png"
-	img2pdf "/tmp/temp-imgs-pdf-conversion/"*".png" -o "/tmp/temp-imgs-pdf-conversion/converted-pdf-out.pdf"
-	cp "/tmp/temp-imgs-pdf-conversion/converted-pdf-out.pdf" "$1"
+	"${running_path_of_script}/ffmpeg-file-cleaner.sh" "${tmp_img_proc_dir}/out-${current_file}"*".png"
+	img2pdf "${tmp_img_proc_dir}/"*".png" -o "${tmp_img_proc_dir}/converted-pdf-out.pdf"
+	mv "${tmp_img_proc_dir}/converted-pdf-out.pdf" "${current_index_of_shell_parameter_expansion}"
+
+	#img2pdf names generated images always the same.
+	#So to avoid all pdfs parsed in one sitting from using the same images, remove the old ones
+	rm "${tmp_img_proc_dir}/"*".png"
 	shift
 done
 
-rm -rf "/tmp/temp-imgs-pdf-conversion"
+rm -rf "${tmp_img_proc_dir}"
